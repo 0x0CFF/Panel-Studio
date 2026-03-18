26118027d10cefe5318453c9ff62d6a6fe49f536
0x0CFF <0x0CFF@proton.me>
2026-03-19 01:01:35 +0800

    修复 BUG

--- static/js/common/initCookie.js
+++ static/js/common/initCookie.js
@@ -1,5 +1,5 @@
 // 检查名为 'tag' 的 cookie 是否存在
-if ($.cookie('tag') === undefined) {
-    // 如果不存在，则创建它，并设置初始值
-    $.cookie('tag', 'DEVELOPMENT');
+if ($.cookie("tag") === undefined) {
+  // 如果不存在，则创建它，并设置初始值
+  $.cookie("tag", "DEVELOPMENT");
 }
--- static/js/index/search.js
+++ static/js/index/search.js
@@ -1,36 +1,36 @@
 // 键盘搜索触发器
-document.getElementById('search').addEventListener('keydown', function(event) {
-    // 获取搜索引擎
-    engine = $.cookie("engine_cookie")
-    // console.log(engine)
-    // 检查回车键是否按下
-    if (event.key === 'Enter') {
-        const inputValue = document.getElementById('search').value;
-        if (engine == "Baidu") {
-            window.location.href = `https://www.baidu.com/s?wd=${inputValue}`;
-        } else if (engine == "Bing") {
-            window.location.href = `https://www.bing.com/search?q=${inputValue}`;
-        } else if (engine == "Google") {
-            window.location.href = `https://www.google.com/search?q=${inputValue}`;
-        } else if (engine == "Duckduckgo") {
-            window.location.href = `https://duckduckgo.com/?q=${inputValue}`;
-        }
+document.getElementById("search").addEventListener("keydown", function (event) {
+  // 获取搜索引擎
+  engine = $.cookie("engine");
+  // console.log(engine);
+  // 检查回车键是否按下
+  if (event.key === "Enter") {
+    const inputValue = document.getElementById("search").value;
+    if (engine == "Baidu") {
+      window.location.href = `https://www.baidu.com/s?wd=${inputValue}`;
+    } else if (engine == "Bing") {
+      window.location.href = `https://www.bing.com/search?q=${inputValue}`;
+    } else if (engine == "Google") {
+      window.location.href = `https://www.google.com/search?q=${inputValue}`;
+    } else if (engine == "Duckduckgo") {
+      window.location.href = `https://duckduckgo.com/?q=${inputValue}`;
     }
+  }
 });
 
 // 鼠标搜索触发器
-document.getElementById('mcp').addEventListener('click', function(event) {
-    // 获取搜索引擎
-    engine = $.cookie("engine_cookie")
-    // console.log(engine)
-    const inputValue = document.getElementById('search').value;
-    if (engine == "Baidu") {
-        window.location.href = `https://www.baidu.com/s?wd=${inputValue}`;
-    } else if (engine == "Bing") {
-        window.location.href = `https://www.bing.com/search?q=${inputValue}`;
-    } else if (engine == "Google") {
-        window.location.href = `https://www.google.com/search?q=${inputValue}`;
-    } else if (engine == "Duckduckgo") {
-        window.location.href = `https://duckduckgo.com/?q=${inputValue}`;
-    }
+document.getElementById("mcp").addEventListener("click", function (event) {
+  // 获取搜索引擎
+  engine = $.cookie("engine");
+  // console.log(engine)
+  const inputValue = document.getElementById("search").value;
+  if (engine == "Baidu") {
+    window.location.href = `https://www.baidu.com/s?wd=${inputValue}`;
+  } else if (engine == "Bing") {
+    window.location.href = `https://www.bing.com/search?q=${inputValue}`;
+  } else if (engine == "Google") {
+    window.location.href = `https://www.google.com/search?q=${inputValue}`;
+  } else if (engine == "Duckduckgo") {
+    window.location.href = `https://duckduckgo.com/?q=${inputValue}`;
+  }
 });
